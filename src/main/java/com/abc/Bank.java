84d1b2fb083aa52ba32ab4e441117a6fc6ed5de8
Rahul Akarte <rakarte@yahoo.com>
2016-01-19 11:18:30 -0500

    Undid the static customers change as each bank will have its own list of customers.

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bank {
-    private static List<Customer> customers;
+    private List<Customer> customers;
 
     public Bank() {
         customers = new ArrayList<Customer>();
